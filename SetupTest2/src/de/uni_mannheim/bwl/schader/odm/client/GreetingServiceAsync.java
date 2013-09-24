66f66ebc4a7452cabfd47adaa1528d83a7a9ff34
pakunz <pakunz@students.mail.uni-mannheim.de>
2013-09-24 14:50:52 +0200

    testcommit

---
+++ SetupTest2/src/de/uni_mannheim/bwl/schader/odm/client/GreetingServiceAsync.java
@@ -0,0 +1,11 @@
+package de.uni_mannheim.bwl.schader.odm.client;
+
+import com.google.gwt.user.client.rpc.AsyncCallback;
+
+/**
+ * The async counterpart of <code>GreetingService</code>.
+ */
+public interface GreetingServiceAsync {
+	void greetServer(String input, AsyncCallback<String> callback)
+			throws IllegalArgumentException;
+}
